705a86718516cbf16e550df047559e5b04e07d04
Bernardo Baquero Stand <baquerober@gmail.com>
2017-05-21 15:16:42 +0200

    Added <ShowAt> component :raised_hands:
    ...by abstracting the main functionality into a private stateful component, and only exposing <HideAt> and <ShowAt> as stateless components.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-import HideAt from './HideAt';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export { HideAt };
+/**
+ * Descriptively hide or show children components, based on a breakpoint
+ * Uses matchMedia
+ * Client-side only
+ */
+
+class HideShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+    this.mql = window.matchMedia(props.breakpoint);
+    this.mql.addListener(this.updateVisibility.bind(this));
+  }
+
+  updateVisibility() {
+    const breakpointActive = this.mql.matches;
+    if (this.props.hide) {
+      this.setState({ visible: !breakpointActive });
+    } else {
+      this.setState({ visible: breakpointActive });
+    }
+  }
+
+  // Check visibility before mounting (ergo `render()`-ing)
+  // so it does not render `children` if the breakpoint is already active
+  componentWillMount() {
+    this.updateVisibility();
+  }
+
+  render() {
+    return this.state.visible ? this.props.children : null;
+  }
+}
+
+const commonPropTypes = {
+  breakpoint: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+HideShow.propTypes = {
+  hide: PropTypes.bool.isRequired,
+};
+
+const HideAt = ({ breakpoint, children }) => (
+  <HideShow breakpoint={breakpoint} hide={true}>
+    {children}
+  </HideShow>
+);
+
+HideAt.propTypes = commonPropTypes;
+
+const ShowAt = ({ breakpoint, children }) => (
+  <HideShow breakpoint={breakpoint} hide={false}>
+    {children}
+  </HideShow>
+);
+
+ShowAt.propTypes = commonPropTypes;
+
+export { HideAt, ShowAt };
